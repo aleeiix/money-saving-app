86e1b2d99eddf5cdb04b14ee48bf518afb5a667e
Aleix Riba <aleixriba33@gmail.com>
2021-07-24 23:05:14 +0200

    add displayName in Home

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { State } from '../../models/interfaces/state'
 
 const Home: FC = () => {
+	const userLogged = useSelector((state: State) => state.userLogged)
+
 	return (
 		<div>
-			<h1>Hola </h1>
+			<h1>Hola {userLogged?.displayName}</h1>
 
 			<h5>Resumen del mes / Configuracion</h5>
 
